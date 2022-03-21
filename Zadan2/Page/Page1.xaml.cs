3204a0d15df2a70c9aa1211e211b33222deb8d37
Pollyam <101092550+Pollyam@users.noreply.github.com>
2022-03-21 09:32:19 +0300

    Add files via upload

---
+++ Zadan2/Page/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadan2.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(textbox1.Text);
+            double b = double.Parse(textbox2.Text);
+            double c = double.Parse(textbox3.Text);
+            double d = b * b - (4 * a * c);
+            double x1 = (b * (-1) - Math.Sqrt(d)) / (2 * a);
+            double x2 = (b * (-1) - Math.Sqrt(d)) / (2 * a);
+
+        }
+
+    }
+}
---
+++ Zadan2/Page/Page2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadan2.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double k = double.Parse(textbox1.Text);
+            double b = double.Parse(textbox2.Text);
+            double d = k * b + m ;
+        }
+    }
+}
